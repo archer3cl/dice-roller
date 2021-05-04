78ef71c8f3e2ace17246879fc523d3d88840603a
Alex Vieyra <vieyrafc@gmail.com>
2021-05-04 01:53:42 -0500

    Fix useEffect references on displaying die result

--- src/components/rollDie.js
+++ src/components/rollDie.js
@@ -5,14 +5,7 @@ export default function RollDie({ faces, roll }) {
   const [state, setState] = useState('');
   const [displayValue, setDisplayValue] = useState(1);
   const [result, setResult] = useState();
-  const dieValues = Array.from({ length: faces }, (_, i) => i + 1);
   const prevResultRef = useRef();
-
-  useEffect(() => {
-    prevResultRef.current = result;
-  });
-
-  const prevCount = prevResultRef.current;
 
   useEffect(() => {
     if (roll) {
@@ -28,21 +21,29 @@ export default function RollDie({ faces, roll }) {
 
     if (state === 'ROLLING') {
       intervalId = setInterval(() => {
+        const dieValues = Array.from({ length: faces }, (_, i) => i + 1);
         setDisplayValue(Math.ceil(Math.random() * dieValues.length));
       }, 50);
     }
 
     return () => clearInterval(intervalId);
-  }, [state, displayValue, dieValues]);
+  }, [state, displayValue, faces]);
+
+  useEffect(() => {
+    prevResultRef.current = result;
+  });
 
   useEffect(() => {
     if (state === 'RESULT') {
-      if (prevCount && dieValues.length > 1) {
-        dieValues.splice(dieValues.indexOf(prevCount), 1);
+      const dieValues = Array.from({ length: faces }, (_, i) => i + 1);
+      const prevResult = prevResultRef.current;
+      if (prevResult && dieValues.length > 1) {
+        dieValues.splice(dieValues.indexOf(prevResult), 1);
       }
-      setResult(dieValues[Math.floor(Math.random() * dieValues.length)]);
+      var newResult = dieValues[Math.floor(Math.random() * dieValues.length)];
+      setResult(newResult);
     }
-  }, [state]);
+  }, [state, faces]);
 
   if (state === 'ROLLING') return <Die value={displayValue} />;
 
